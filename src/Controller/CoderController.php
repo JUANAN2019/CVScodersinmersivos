716ae12ec5ed8e9a5812cdac45a737c8ba011ba1
JUANAN2019 <garciagalindo@telefonica.net>
2023-09-15 14:54:02 +0200

    controller coder index y show, los demas comentados, coder index como home  y añadidas competencias a twig de index coder

--- src/Controller/CoderController.php
+++ src/Controller/CoderController.php
@@ -5,42 +5,44 @@ namespace App\Controller;
 use App\Entity\Coder;
 use App\Form\CoderType;
 use App\Repository\CoderRepository;
+use App\Repository\CompetenceRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/coder')]
+#[Route('/')]
 class CoderController extends AbstractController
 {
     #[Route('/', name: 'app_coder_index', methods: ['GET'])]
-    public function index(CoderRepository $coderRepository): Response
+    public function index(CoderRepository $coderRepository,CompetenceRepository$competenceRepository ): Response
     {
         return $this->render('coder/index.html.twig', [
             'coders' => $coderRepository->findAll(),
+            'competences' => $competenceRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_coder_new', methods: ['GET', 'POST'])]
-    public function new(Request $request, EntityManagerInterface $entityManager): Response
-    {
-        $coder = new Coder();
-        $form = $this->createForm(CoderType::class, $coder);
-        $form->handleRequest($request);
+    // #[Route('/new', name: 'app_coder_new', methods: ['GET', 'POST'])]
+    // public function new(Request $request, EntityManagerInterface $entityManager): Response
+    // {
+    //     $coder = new Coder();
+    //     $form = $this->createForm(CoderType::class, $coder);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($coder);
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->persist($coder);
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
-        }
+    //         return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
+    //     }
 
-        return $this->renderForm('coder/new.html.twig', [
-            'coder' => $coder,
-            'form' => $form,
-        ]);
-    }
+    //     return $this->renderForm('coder/new.html.twig', [
+    //         'coder' => $coder,
+    //         'form' => $form,
+    //     ]);
+    // }
 
     #[Route('/{id}', name: 'app_coder_show', methods: ['GET'])]
     public function show(Coder $coder): Response
@@ -50,32 +52,32 @@ class CoderController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_coder_edit', methods: ['GET', 'POST'])]
-    public function edit(Request $request, Coder $coder, EntityManagerInterface $entityManager): Response
-    {
-        $form = $this->createForm(CoderType::class, $coder);
-        $form->handleRequest($request);
+    // #[Route('/{id}/edit', name: 'app_coder_edit', methods: ['GET', 'POST'])]
+    // public function edit(Request $request, Coder $coder, EntityManagerInterface $entityManager): Response
+    // {
+    //     $form = $this->createForm(CoderType::class, $coder);
+    //     $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->flush();
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         $entityManager->flush();
 
-            return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
-        }
+    //         return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
+    //     }
 
-        return $this->renderForm('coder/edit.html.twig', [
-            'coder' => $coder,
-            'form' => $form,
-        ]);
-    }
+    //     return $this->renderForm('coder/edit.html.twig', [
+    //         'coder' => $coder,
+    //         'form' => $form,
+    //     ]);
+    // }
 
-    #[Route('/{id}', name: 'app_coder_delete', methods: ['POST'])]
-    public function delete(Request $request, Coder $coder, EntityManagerInterface $entityManager): Response
-    {
-        if ($this->isCsrfTokenValid('delete'.$coder->getId(), $request->request->get('_token'))) {
-            $entityManager->remove($coder);
-            $entityManager->flush();
-        }
+    // #[Route('/{id}', name: 'app_coder_delete', methods: ['POST'])]
+    // public function delete(Request $request, Coder $coder, EntityManagerInterface $entityManager): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$coder->getId(), $request->request->get('_token'))) {
+    //         $entityManager->remove($coder);
+    //         $entityManager->flush();
+    //     }
 
-        return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
-    }
+    //     return $this->redirectToRoute('app_coder_index', [], Response::HTTP_SEE_OTHER);
+    // }
 }
